9fd9fd19208e38b1522940155331c0ab11d5437a
ajdevmaster <ajdevmaster@hotmail.com>
2022-02-19 16:22:54 -0300

    atualizacao

--- BancoRoxinho/BancoRoxinho.Dominio/Model/PessoaJuridica.cs
+++ BancoRoxinho/BancoRoxinho.Dominio/Model/PessoaJuridica.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using CPFCNPJ;
 
 namespace BancoRoxinho.Dominio.Model
 {
+    //PessoaJuritica herdando Pessoa
     public class PessoaJuridica : Pessoa
     {
-        public PessoaJuridica CadastrarPessoa()
+        public string CNPJ = "00.000.000/0000-00";
+        public string Nome;
+
+        bool VerificarCNPJ()
         {
-            throw new NotImplementedException();
+            var verificador = new Main();
+            var cnpjValido = verificador.IsValidCPFCNPJ(CNPJ);
+            if(!cnpjValido)
+            {
+                Console.WriteLine("CNPJ Inválido");
+                return false;
+            }
+            return cnpjValido;
+        }
+    public PessoaJuridica CadastrarPessoaJuridica(PessoaJuridica pessoa)
+        {
+            var pessoaJuridica = new PessoaJuridica();
+
+            Console.WriteLine("Digite o nome da Empresa: ");
+            pessoa.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o CNPJ da Empresa: " + pessoaJuridica.CNPJ + ":");
+            pessoaJuridica.CNPJ = Console.ReadLine();
+
+
+            bool cnpjValido = VerificarCNPJ(PessoaJuridica.CNPJ);
+            if (!cnpjValido)
+            {
+                Console.WriteLine("CNPJ Inválido");
+                return null;
+            }
+
+            return pessoa;
         }
     }
 }
