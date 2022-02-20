720bfc0e9d0ca1739b2738798212412564c0f1fa
celso <celsohg@gmail.com>
2022-02-20 20:57:25 -0300

    Celso - incluido cadastro pessoa juridica

--- BancoRoxinho/BancoRoxinho.Dominio/Model/PessoaJuridica.cs
+++ BancoRoxinho/BancoRoxinho.Dominio/Model/PessoaJuridica.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using CPFCNPJ;
 
 namespace BancoRoxinho.Dominio.Model
 {
+    // Pessoa Juridica herda Pessoa
+    
     public class PessoaJuridica : Pessoa
     {
-        public PessoaJuridica CadastrarPessoa()
+
+        public string CNPJ = "00.000.000/0000-00";
+        public string Nome;
+
+        bool VerificarCNPJ(string cnpjASerValdido)
         {
-            throw new NotImplementedException();
+            var verificador = new Main();
+            var cnpjValido = verificador.IsValidCPFCNPJ(cnpjASerValdido);
+            return cnpjValido;
+        }
+
+        public PessoaJuridica CadastrarPessoaj()
+        {
+            var pessoa = new PessoaJuridica();
+            Console.WriteLine("Digite o nome da pessoa Juridica: ");
+            pessoa.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o CNPJ de " + pessoa.Nome + ":");
+            pessoa.CNPJ = Console.ReadLine();
+
+            bool cnpjValido = VerificarCNPJ(pessoa.CNPJ);
+            if (!cnpjValido)
+            {
+                Console.WriteLine("CNPJ inválido.");
+                return null;
+            }
+            Console.Clear();
+            return pessoa;
         }
     }
 }
