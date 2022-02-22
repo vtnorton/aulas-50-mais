a92d8bb1a34fd0c00e7b4f41d653c6983d3c74cf
jaquesonalves <jaquesonalves@hotmail.com>
2022-02-21 22:59:28 -0300

    Add Pessoa Jurídica

--- BancoRoxinho/BancoRoxinho.Dominio/Program.cs
+++ BancoRoxinho/BancoRoxinho.Dominio/Program.cs
@@ -8,61 +8,43 @@ namespace BancoRoxinho.Dominio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bem vindo ao banco Roxinho");
-            Console.WriteLine("Escolha um número para prosseguir:");
-            Console.WriteLine("1 - Cadastrar pessoa física");
-            Console.WriteLine("2 - Ler pessoas físicas cadastrada");
-            Console.WriteLine("3 - Cadastrar pessoa Jurídica");
-            Console.WriteLine("4 - Ler pessoas Juridicas cadastrada");
-
-            int escolhaDoUsuario = int.Parse(Console.ReadLine());
-
-           
-            switch(escolhaDoUsuario)
+            bool continuarNoPrograma = true;
+            do
             {
-                case 1:
-                    EscolheuAOpcaoCadastrarPessoa();
-                    break;
+                Console.WriteLine("Bem vindo ao banco Roxinho");
+                Console.WriteLine("Escolha um número para prosseguir:");
+                Console.WriteLine("1 - Cadastrar pessoa física");
+                Console.WriteLine("2 - Ler pessoas físicas cadastrada");
+                Console.WriteLine("3 - Cadastrar pessoa Jurídica");
+                Console.WriteLine("4 - Ler pessoas Juridicas cadastrada");
+                Console.WriteLine("0 - Sair");
 
-                case 2:
-                    EscolheuAOpcaoDeVerPessoasFisicas();
-                    break;
+                int escolhaDoUsuario = int.Parse(Console.ReadLine());
 
-                case 3:
-                    EscolheuAOpcaoCadastrarPessoaJuridica();
-                    break;                  
-            }
+                switch (escolhaDoUsuario)
+                {
+                    case 1:
+                        EscolheuAOpcaoCadastrarPessoa();
+                        break;
 
+                    case 2:
+                        EscolheuAOpcaoDeVerPessoasFisicas();
+                        break;
 
-           // if(escolhaDoUsuario == 1)
-           // {
-            //    EscolheuAOpcaoCadastrarPessoa();
-           // }
+                    case 3:
+                        EscolheuAOpcaoCadastrarPessoaJuridica();
+                        break;
 
-           // if(escolhaDoUsuario == 2)
-           // {
-           //     EscolheuAOpcaoDeVerPessoasFisicas();
-           // }
+                    case 4:
+                        EscolheuAOpcaoDeVerPessoasJuridica();
+                        break ;
 
-            Console.Read();
-        }
-
-        static void EscolheuAOpcaoDeVerPessoasFisicas()
-        {
-            var listaPessoas = PessoasRepository.PessoasFisicas;
-            
-            // Explicar melhor na próxima aula
-            for (int contador = 0; contador <= listaPessoas.Count; contador++)
-            {
-                // Explicar melhor na próxima aula
-                var pessoa = listaPessoas[contador];
-                Console.WriteLine("");
-                Console.WriteLine("Pessoa de Nº " + contador);
-                Console.WriteLine("Nome da pessoa: " + pessoa.Nome);
-                Console.WriteLine("Idade da pessoa: " + pessoa.Idade);
-                Console.WriteLine("Conta da pessoa: " + pessoa.ContaCorrente.NumeroDaConta);
-                Console.WriteLine("");
-            }
+                    case 0:
+                    default:
+                        continuarNoPrograma = false;
+                        break;
+                }
+            } while (continuarNoPrograma); 
         }
 
         static void EscolheuAOpcaoCadastrarPessoa()
@@ -73,12 +55,42 @@ namespace BancoRoxinho.Dominio
             PessoasRepository.PessoasFisicas.Add(pessoaCadastrada);
         }
 
+        static void EscolheuAOpcaoDeVerPessoasFisicas()
+        {
+            var listaPessoas = PessoasRepository.PessoasFisicas;
+            var pessoa = new PessoaFisica();
+
+            listaPessoas.ForEach(pessoa =>
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Pessoa de Nº ");
+                Console.WriteLine("Nome da pessoa: " + pessoa.Nome);
+                Console.WriteLine("Idade da pessoa: " + pessoa.Idade);
+                Console.WriteLine("Conta da pessoa: " + pessoa.ContaCorrente.NumeroDaConta);
+                Console.WriteLine("");
+            });
+        }
+
         static void EscolheuAOpcaoCadastrarPessoaJuridica()
         {
             var pessoaJuridica = new PessoaJuridica();
-            var pessoaCadastrada = pessoaJuridica.CadastrarPessoaJuridica();
+            var pessoaJuridicaCadastrada = pessoaJuridica.CadastrarPessoaJuridica();
 
-            PessoasRepository.PessoaJuridicas.Add(pessoaCadastrada);
+            PessoasRepository.PessoasJuridicas.Add(pessoaJuridicaCadastrada);
+        }
+
+        static void EscolheuAOpcaoDeVerPessoasJuridica()
+        {
+            var listaPessoaJuridica = PessoasRepository.PessoasJuridicas;
+            var Juridica = new PessoaJuridica();
+
+            listaPessoaJuridica.ForEach(Juridica =>
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Nome da pessoa: " + Juridica.Nome);
+                Console.WriteLine("CNPJ da Empresa: " + Juridica.CNPJ);
+                Console.WriteLine("");
+            });
         }
     }
 }
