779ea5972168c78893a74786d473be9f0f285a10
vivianemk <kikulevmk@gmail.com>
2022-02-22 22:26:22 -0300

    Feat : Incluido teste pessoa fisica e juridica

---
+++ BancoRoxinho/BancoRoxinho.Dominio.Test/PessoaJuridicaTest.cs
@@ -0,0 +1,26 @@
+﻿using BancoRoxinho.Dominio.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BancoRoxinho.Dominio.Test
+{
+    
+    public class PessoaJuridicaTest
+    {
+        [Fact]
+        public void DeveValidarNumeroCnpj()
+        {
+            string CNPJ = "55645180000150";
+            bool cnpjValidacaoEsperado = true;
+            var pessoa = new PessoaJuridica();
+
+            var resultado = pessoa.VerificarCNPJ(CNPJ);
+            Assert.True(resultado == cnpjValidacaoEsperado);
+
+        }
+    }
+}
